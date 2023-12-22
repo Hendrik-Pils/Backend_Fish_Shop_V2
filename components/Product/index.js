2fe966dde832a0173ca95fc4f0abfe83887ed385
Hendrik Pils <hendrik-pils@gmx.de>
2023-12-22 11:57:54 +0100

    finish challenge

--- components/Product/index.js
+++ components/Product/index.js
@@ -1,14 +1,52 @@
 import useSWR from "swr";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
 import Comments from "../Comments";
+import ProductForm from "../ProductForm";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    // we need to figure out what is in the form
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    // console.log("jokeData: ", jokeData);
+    // we need to make a PUT request to our API route
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct() {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      await response.json();
+      // then we should redirect to the homepage
+      router.push("/");
+    } else console.log(response.status);
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -21,6 +59,27 @@ export default function Product() {
   return (
     <ProductCard>
       <h2>{data.name}</h2>
+      <button
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        <span role="img" aria-label="A pencil">
+          ✏️
+        </span>
+      </button>
+      <button onClick={() => handleDeleteProduct(id)} disabled={isEditMode}>
+        <span role="img" aria-label="A cross indicating deletion">
+          ❌
+        </span>
+      </button>
+      {isEditMode && (
+        <ProductForm
+          onSubmit={handleEditProduct}
+          value={data.product}
+          isEditMode={true}
+        />
+      )}
       <p>Description: {data.description}</p>
       <p>
         Price: {data.price} {data.currency}
